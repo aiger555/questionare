e457237d5761584490d22a14cb58e4fa2f3e1d23
Aigerim <aigerim.nuralieva@iaau.edu.kg>
2024-10-16 19:38:48 +0600

    q visible for user

--- src/main/java/com/example/questionare/controllers/QController.java
+++ src/main/java/com/example/questionare/controllers/QController.java
@@ -1,13 +1,14 @@
 package com.example.questionare.controllers;
 
+import com.example.questionare.models.Question;
+import com.example.questionare.models.QuestionWrapper;
 import com.example.questionare.services.QService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("questionare")
@@ -20,5 +21,10 @@ public class QController {
     public ResponseEntity<String> createQ(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return qService.createQ(category, numQ, title);
 
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQquestions(@PathVariable Integer id){
+        return qService.getQquestions(id);
     }
 }
--- src/main/java/com/example/questionare/services/QService.java
+++ src/main/java/com/example/questionare/services/QService.java
@@ -4,12 +4,15 @@ import com.example.questionare.dao.QDao;
 import com.example.questionare.dao.QuestionDao;
 import com.example.questionare.models.Q;
 import com.example.questionare.models.Question;
+import com.example.questionare.models.QuestionWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QService {
@@ -30,5 +33,26 @@ public class QService {
 
         return new ResponseEntity<>("success", HttpStatus.CREATED);
 
+    }
+
+
+    public ResponseEntity<List<QuestionWrapper>> getQquestions(Integer id) {
+        Optional<Q> q = qDao.findById(id);
+        List<Question> questionsFromDB = q.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for(Question qs : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(
+                    qs.getId(),
+                    qs.getQuestion_title(),
+                    qs.getOption1(),
+                    qs.getOption2(),
+                    qs.getOption3());
+            questionsForUser.add(qw);
+
+
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
